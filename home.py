79a8f498e79141fd294b994faedd8921cee6cab9
Shuning Bian <freespace@gmail.com>
2013-06-14 15:14:57 +0100

    Splits identify.py into identify.py and home.py

--- home.py
+++ home.py
@@ -9,6 +9,10 @@ def main(args):
   print 'Looking for APT controllers'
   drv = pylibftdi.Driver()
   controllers = drv.list_devices()
+  serial = args[1]
+
+  if serial:
+    controllers = filter(lambda x:x[2] == serail, controllers)
 
   if controllers:
     for con in controllers:
@@ -16,6 +20,12 @@ def main(args):
       with pyAPT.Controller(serial_number=con[2]) as con:
         print '\tIdentifying controller'
         con.identify()
+        print '\tResetting controller parameters'
+        con.reset_params()
+        print '\tHoming parameters:', con.request_home_params()
+        print '\tHoming stage...',
+        con.home(velocity=1)
+        print 'homed'
 
       return 0
   else:
