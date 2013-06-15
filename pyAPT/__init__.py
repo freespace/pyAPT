ff4f61a9d42e26b4f089cee2a0821ec0aceaa9e3
Shuning Bian <freespace@gmail.com>
2013-06-15 21:41:57 +0100

    Adds MTS50 to __all___, prepends 0xfaf0 to USB_PID_LIST for faster open (__init__.py)

--- pyAPT/__init__.py
+++ pyAPT/__init__.py
@@ -1,9 +1,13 @@
 __version__ = "0.01"
 __author__ = "Shuning Bian"
 
-__all__ = ['Message', 'Controller']
+__all__ = ['Message', 'Controller', 'MTS50']
 
-from pyAPT import message, controller
+from pyAPT import message, controller, MTS50
 
 Message = message.Message
 Controller = controller.Controller
+MTS50 = MTS50.MTS50
+
+import pylibftdi
+pylibftdi.USB_PID_LIST.insert(0,0xfaf0)
