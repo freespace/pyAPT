13e61d9059dd081f7257ce91f6f1f242205d88cb
Shuning Bian <freespace@gmail.com>
2013-06-15 21:42:50 +0100

    Adds a bench.py to perform rudimentary benchmarking of various functions

---
+++ bench.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""
+Usage: python bench.py
+
+Performs simply benchmarks on how long it takes to list devices, open a device
+and performing a status query
+"""
+
+import pylibftdi
+import pyAPT
+import time
+
+def main(args):
+  print 'Looking for APT controllers'
+  drv = pylibftdi.Driver()
+
+  st = time.time()
+  controllers = drv.list_devices()
+  print '\tlist_devices:',time.time()-st
+
+  if controllers:
+    for con in controllers:
+      print 'Found %s %s S/N: %s'%con
+      st = time.time()
+      with pyAPT.Controller(serial_number=con[2]) as con:
+        print '\topen:',time.time()-st
+        st = time.time()
+        status = con.status()
+        print '\tstatus:',time.time()-st
+
+        print '\tController status:'
+        print '\t\tPosition: %.2fmm'%(status.position)
+        print '\t\tVelocity: %.2fmm'%(status.velocity)
+        print '\t\tStatus:',status.flag_strings()
+
+      return 0
+  else:
+    print '\tNo APT controllers found. Maybe you need to specify a PID'
+    return 1
+
+
+if __name__ == '__main__':
+  import sys
+  sys.exit(main(sys.argv))
