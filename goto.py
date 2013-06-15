f2c8b10ebfb1b3c1393f285fd338d637a27bbc8a
Shuning Bian <freespace@gmail.com>
2013-06-15 11:50:14 +0100

    Adds goto.py which moves the stage to an absolute position

---
+++ goto.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Usage: python goto.py <serial> <position>
+
+This program tells the specified controller to move the stage to the specified
+position.
+"""
+
+import time
+import pylibftdi
+import pyAPT
+
+def main(args):
+  if len(args)<3:
+    print __doc__
+    return 1
+  else:
+    serial = args[1]
+    position = int(args[2])
+
+  try:
+    with pyAPT.Controller(serial_number=serial) as con:
+      print 'Found APT controller S/N',serial
+      print '\tMoving stage to %dmm...'%(position),
+      con.goto(position)
+      print 'moved'
+      return 0
+  except Exception  as ex:
+    print '\tCould not find APT controller S/N of',serial
+    return 1
+
+if __name__ == '__main__':
+  import sys
+  sys.exit(main(sys.argv))
+
