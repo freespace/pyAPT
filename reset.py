d6aaa6aaa51e389e57ff93aac920d63dcf907d80
Shuning Bian <freespace@gmail.com>
2013-06-17 11:15:15 +0100

    Converts reset.py to runner pattern

--- reset.py
+++ reset.py
@@ -4,39 +4,17 @@ Usage: python reset.py [<serial>]
 
 Resets all controller parameters to their default EEPROM value.
 """
-import pylibftdi
 import pyAPT
 
+from runner import runner_serial
+
+@runner_serial
 def reset(serial):
     with pyAPT.Controller(serial_number=serial) as con:
       print '\tResetting controller parameters to EEPROM defaults'
       con.reset_parameters()
 
-def main(args):
-  if len(args)>1:
-    serial = args[1]
-  else:
-    serial = None
-
-  if serial:
-    reset(serial)
-    return 0
-  else:
-    print 'Looking for APT controllers'
-    drv = pylibftdi.Driver()
-    controllers = drv.list_devices()
-
-    if controllers:
-      for con in controllers:
-        print 'Found %s %s S/N: %s'%con
-        reset(con[2])
-
-      return 0
-    else:
-      print '\tNo APT controllers found. Maybe you need to specify a PID'
-      return 1
-
 if __name__ == '__main__':
   import sys
-  sys.exit(main(sys.argv))
+  sys.exit(reset())
 
