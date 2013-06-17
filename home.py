80eb8801936eb825c620b0fd803fec643f9437ba
Shuning Bian <freespace@gmail.com>
2013-06-17 11:12:24 +0100

    Converts home.py to new runner pattern

--- home.py
+++ home.py
@@ -6,43 +6,21 @@ This program homes all APT controllers found, or of the one specified
 """
 
 import time
-import pylibftdi
 import pyAPT
 
+from runner import runner_serial
+
+@runner_serial
 def home(serial):
   with pyAPT.Controller(serial_number=serial) as con:
     print '\tIdentifying controller'
     con.identify()
     print '\tHoming parameters:', con.request_home_params()
     print '\tHoming stage...',
-    con.home()
+    con.home(velocity=2)
     print 'homed'
-
-def main(args):
-  if len(args)>1:
-    serial = args[1]
-  else:
-    serial = None
-
-  if serial:
-    home(serial)
-    return 0
-  else:
-    print 'Looking for APT controllers'
-    drv = pylibftdi.Driver()
-    controllers = drv.list_devices()
-
-    if controllers:
-      for con in controllers:
-        print 'Found %s %s S/N: %s'%con
-        home(con[2])
-
-      return 0
-    else:
-      print '\tNo APT controllers found. Maybe you need to specify a PID'
-      return 1
 
 if __name__ == '__main__':
   import sys
-  sys.exit(main(sys.argv))
+  sys.exit(home())
 
