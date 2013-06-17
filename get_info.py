6f438e1c73abff4d4fdd43d5aa9d0e65547c0e53
Shuning Bian <freespace@gmail.com>
2013-06-17 12:01:00 +0100

    Adds get_info.py to get information about each controller

---
+++ get_info.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Usage: python get_info.py [<serial>]
+
+Gets the controller information of all APT controllers, or the one specified
+"""
+
+import pyAPT
+
+from runner import runner_serial
+
+@runner_serial
+def info(serial):
+  with pyAPT.Controller(serial_number=serial) as con:
+    info = con.info()
+    print '\tController info:'
+    labels=['S/N','Model','Type','Firmware Ver', 'Notes', 'H/W Ver',
+            'Mod State', 'Channels']
+
+    for idx,ainfo in enumerate(info):
+      print '\t%12s: %s'%(labels[idx], bytes(ainfo))
+
+if __name__ == '__main__':
+  import sys
+  sys.exit(info())
+
+
+
